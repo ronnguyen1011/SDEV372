55c275d659dd6b004bd2b4cdf672a451c77773fc
Ron Nguyen <ronnguyen1011@gmail.com>
2023-09-28 18:15:49 -0700

    Reset Branch and merger Reverted to a previous version of my Point class. Committing changes to alter the (x, y) coordinate to an (x, y, z) coordinate.

--- TestProject/src/making_changes/Point.java
+++ TestProject/src/making_changes/Point.java
@@ -6,38 +6,12 @@ public class Point {
     private double x;
     private double y;
 
-    public Point(double x, double y) {
+    private double z;
+
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
+        this.z = z;
     }
 
     @Override
@@ -45,6 +19,7 @@ public class Point {
         return "Point{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
                 '}';
     }
 }
